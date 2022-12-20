7017854db1ceceb9e50a8b5be73f26daa8e431ae
anwuna <kevinagentz@yahoo.com>
2022-12-20 09:56:04 -0800

    initial commit

---
+++ ArchitectureDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArchitectureDemo
+//
+//  Created by Chibundu Anwuna on 2022-12-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
